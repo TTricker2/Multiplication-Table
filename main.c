331358a6894a6b856c9090239323cbcdc6518ab8
Your Name <jhu5689@naver.com>
2017-07-16 21:12:45 +0900

    second commit

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
-/* 
+/*
 C 멘토링 - 구구단게임  과제
-*/ 
+*/
+#define _CRT_SECURE_NO_WARNIGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h> 
@@ -9,37 +10,51 @@ C 멘토링 - 구구단게임  과제
 
 void Start() // 게임 시작함수 
 {
-	SetConsoleTextAttribute( GetStdHandle( STD_OUTPUT_HANDLE ), 13); 
-	printf("구구단 게임을 시작합니다~! \n\n\n");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 13);
+	printf("구구단 게임을 시작합니다~! 한 문제 당 제한시간은 3초! \n\n\n");
 	Sleep(1000); //시간 지연 
-	
-	SetConsoleTextAttribute( GetStdHandle( STD_OUTPUT_HANDLE ), 10); 
+
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
 	printf("3 \n\n");
 	Sleep(1000);
-	SetConsoleTextAttribute( GetStdHandle( STD_OUTPUT_HANDLE ), 14); 
-	printf("2 \n\n"); 
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
+	printf("2 \n\n");
 	Sleep(1000);
-	SetConsoleTextAttribute( GetStdHandle( STD_OUTPUT_HANDLE ), 11); 
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 11);
 	printf("1 \n\n");
 	Sleep(1000);
 }
 
-void MultiplicationProblem()
+int CalculateScore(int num1, int num2) //점수 계산 함수
 {
-	int i=0;
-	srand(time(NULL)); 
-	for(i=0; i<10; i++)
+
+}
+
+void MultiplicationProblem() // 무작위 구구단문제
+{
+	int i = 0;
+	int sol;
+
+	srand(time(NULL));
+	for (i = 0; i<10; i++)
 	{
-		SetConsoleTextAttribute( GetStdHandle( STD_OUTPUT_HANDLE ), (rand() % 15)+1 ); 
-		printf("%d * %d = \n\n\n", (rand() % 8)+2,(rand() % 9)+1);
+		int a = (rand() % 8) + 2;
+		int b = (rand() % 9) + 1;
+		int solution = a*b;
+
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (rand() % 8) + 7);
+		printf("%d * %d = ", a, b);
 		Sleep(3000);
+
+		printf("\n\n\n");
 	}
-} 
+}
+
 
 int main(void)
 {
 	Start();
 	MultiplicationProblem();
-	
-	
-} 
+
+
+}
